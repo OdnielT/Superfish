140587ed30a79381e7a89d83b5180d0b86f91e83
OdnielT <odnielt28@gmail.com>
2026-02-13 15:46:26 +0000

    add react bootstrap

---
+++ src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+function Home() {
+    return ( <HomeStyle>
+        <h1>Superfish</h1>
+    </HomeStyle> );
+}
+
+const HomeStyle = styled.div`
+  
+`;
+
+export default Home;
